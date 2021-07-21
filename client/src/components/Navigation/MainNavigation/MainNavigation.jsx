978bf24e5791c4e293a797817fa7eb012e96bc8a
Vladis Markin <vladismarkin@gmail.com>
2021-07-21 17:54:00 +0300

    Implimenting basic Desktop & Mobile navigation

--- client/src/components/Navigation/MainNavigation/MainNavigation.jsx
+++ client/src/components/Navigation/MainNavigation/MainNavigation.jsx
@@ -1,24 +1,33 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader/MainHeader";
-import NavLinks from "../NavLinks/NavLinks";
+import NavLinks from "./NavLinks/NavLinks";
+import SideDrawer from "../SideDrawer/SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link>Organization Stracture</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link>Organization Stracture</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
